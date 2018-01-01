96e4dc016168f5065d8de1ec58b71a548f151615
Martin <martinetex18@gmail.com>
2018-01-01 22:37:52 +0100

    Instalado react-rating

--- react-animeflv/src/components/AnimeSingleView.jsx
+++ react-animeflv/src/components/AnimeSingleView.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, {Component, Fragment} from 'react'
 import { NavLink } from 'react-router-dom'
 import { Button, Form } from 'semantic-ui-react'
 import axios from 'axios'
@@ -28,24 +28,32 @@ export default class AnimeSingleView extends Component {
             .catch(err => console.log(err))
     }
 
-    handleOnEdit = () => {
+    triggerEditMode = () => {
     	this.setState({
          editMode: !this.state.editMode
       })
 	 }
 
-	 onSaveChange = () => {
-       const sinopsis = this.newSinopsis.value
-       this.setState({editMode: false})
-       axios.put(`/api/animes/${this.state.nombre}`, { sinopsis })
-           .then(res => {
-              if (res.data.error) {
-                 console.log(res.data.error)
-              } else {
-                 const { sinopsis } = res.data
-                 this.setState({ sinopsis })
-              }
-           })
+	 onSubmit = () => {
+       // if (sinopsis !== this.state.sinopsis) {
+			//  axios.put(`/api/animes/${this.state.nombre}`, { sinopsis })
+			// 	  .then(res => {
+			// 		  if (res.data.error) {
+			// 			  console.log(res.data.error)
+			// 		  } else {
+			// 			  const { sinopsis } = res.data
+			// 			  this.setState({ sinopsis, editMode: false })
+			// 		  }
+			// 	  })
+       // } else {
+       //    this.setState({editMode: false})
+       // }
+    }
+
+    handleOnPosterChange = e => {
+       const { poster } = this.state
+       console.log(poster)
+       e.target.previousElementSibling.src = e.target.value
     }
 
 
@@ -53,29 +61,47 @@ export default class AnimeSingleView extends Component {
        const { nombre, poster, sinopsis, puntuacion, isLoading, editMode } = this.state
 
        const EditModeLayout = (
-           <Form>
+           <Form loading={isLoading} onSubmit={this.onSubmit}>
               <Form.Field>
-                 <textarea ref={val => this.newSinopsis = val} placeholder="Sinopsis" defaultValue={sinopsis} />
+					  <input type="text" defaultValue={nombre} ref={val => this.nombre = val}/>
+              </Form.Field>
+              <Form.Field>
+                 <textarea ref={val => this.sinopsis = val} placeholder="Sinopsis" defaultValue={sinopsis} />
+              </Form.Field>
+              <Form.Field>
+					  <img src={poster} alt={`Info sobre ${poster}`}/>
+					  <input onChange={this.handleOnPosterChange} type="text" defaultValue={poster}/>
+              </Form.Field>
+              <Form.Field>
+					  <input type="number" ref={val => this.puntuacion = val} defaultValue={puntuacion} />
+              </Form.Field>
+              <Form.Field>
+                 <Button color="teal" type="submit">Guardar cambios</Button>
               </Form.Field>
            </Form>
        )
 
+       const NormalModeLayout = (
+           <Fragment>
+				  <h2>{nombre}</h2>
+				  <p>{sinopsis}</p>
+				  <img src={poster} alt={`Imagen de ${nombre}`} />
+				  <span>{puntuacion}</span>
+           </Fragment>
+       )
+
        const Controls = (
           <Button.Group>
-             <Button color={editMode ? 'red' : 'green'} onClick={this.handleOnEdit} >{editMode ? 'Cancelar' : 'Editar'}</Button>
+             <Button color={editMode ? 'red' : 'green'} onClick={this.triggerEditMode} >{editMode ? 'Cancelar' : 'Editar'}</Button>
              <Button as={NavLink} primary to="/">Volver a lista</Button>
-             { editMode && <Button color="teal" onClick={this.onSaveChange}>Guardar cambios</Button> }
           </Button.Group>
        )
 
       return (
              isLoading ? <Loader /> :
               <div>
-               {Controls}
-               <h2>{nombre}</h2>
-               { editMode ? EditModeLayout : <p>{sinopsis}</p> }
-                <img src={poster} alt=""/>
-               <span>{puntuacion}</span>
+              {Controls}
+              { editMode ? EditModeLayout : NormalModeLayout }
               </div>
            )
    }
